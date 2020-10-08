b191a0dba8b78718f3431abc59617104b77bedc1
Sanjay <sanjaylavingia@gmail.com>
2020-10-08 02:14:19 -0700

    (added) onHover switch case with working selector, corresponding rtid with tagged id on dom node

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -59,14 +59,9 @@ window.addEventListener('message', ({ data: { action, payload } }: MsgData) => {
       mode.paused = payload;
       break;
     case 'onHover':
-      //find the correct element, add event listener setTimeout 
-      console.log('hover')
-      const numberKeys = document.querySelectorAll('.key-container.number-key');
-      console.log(numberKeys[0].setAttribute('id', 'fromOnHover'));
-      console.log(document.querySelectorAll('.key-container.number-key'));
-      //for loop, if statement,
-      // console.log('WE MADE IT ALL THE WAY FROM THE FRONTEND! HERE\'S THE PAYLOAD:', payload);
-      
+      const myElement = document.getElementById(`${payload}`)
+      myElement.style.backgroundColor = '#4d94ff'
+      setTimeout(() => myElement.style.backgroundColor = "", 1000)
       break;
     default:
       break;
